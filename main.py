1e0efdff91ed7ca637dd9bf20c2995277046eec0
cliffe <oluochcliffe13@gmail.com>
2026-06-17 00:44:13 +0300

    Configure batch orechestration dag

---
+++ main.py
@@ -0,0 +1,30 @@
+from airflow.sdk import dag, task
+from datetime import datetime, timedelta
+from extract import extract
+from load import load
+
+default_args = {
+    'owner': 'Cliffe',
+    'retries': 2,
+    'retry_delay': timedelta(minutes=5),
+    }
+
+@dag(
+    'binance_batch_pipeline',
+    default_args=default_args,
+    description='Batch extraction and load for the Binance pipeline',
+    start_date=datetime(2026, 6, 18),
+    schedule='@daily',
+    max_active_runs=1,
+    catchup=False
+    )
+
+def binance_batch_pipeline():
+
+    @task(task_id='binance_pipeline')
+    def main():
+        load()
+
+    main()
+
+binance_batch_pipeline()
