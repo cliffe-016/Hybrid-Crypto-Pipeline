a2b6f24cde0532a67c08e3538cf3799762893b89
cliffe <oluochcliffe13@gmail.com>
2026-06-24 23:13:01 +0300

    Fix avro conversion error

--- src/processing/spark_schema.py
+++ src/processing/spark_schema.py
@@ -1,40 +1,57 @@
-from pyspark.sql.types import *
+candlestick_avro = """
+{
+  "type": "record", "name": "Envelope", "fields": [
+    { "name": "after", "type": ["null", { "type": "record", "name": "Value", "fields": [
+          {"name": "symbol", "type": ["null", "string"], "default": null},
+          {"name": "open_time", "type": ["null", "long"], "default": null},
+          {"name": "open", "type": ["null", "string"], "default": null},
+          {"name": "high", "type": ["null", "string"], "default": null},
+          {"name": "low", "type": ["null", "string"], "default": null},
+          {"name": "close", "type": ["null", "string"], "default": null},
+          {"name": "volume", "type": ["null", "string"], "default": null}
+        ] }], "default": null }
+  ]
+}
+"""
 
-def wrap_debezium(schema):
-    """Wraps a schema in the Debezium payload envelope"""
-    return StructType([
-                StructField("payload", StructType([
-                    StructField("after", schema, True)
-                    ]), True)
-                ])
+orders_avro = """
+{
+  "type": "record", "name": "Envelope", "fields": [
+    { "name": "after", "type": ["null", { "type": "record", "name": "Value", "fields": [
+          {"name": "symbol", "type": ["null", "string"], "default": null},
+          {"name": "bid_price", "type": ["null", "string"], "default": null},
+          {"name": "bid_qty", "type": ["null", "string"], "default": null},
+          {"name": "ask_price", "type": ["null", "string"], "default": null},
+          {"name": "ask_qty", "type": ["null", "string"], "default": null},
+          {"name": "update_id", "type": ["null", "long"], "default": null}
+        ] }], "default": null }
+  ]
+}
+"""
 
-candlestick_schema = StructType([
-    StructField("open_time", LongType(), True),
-    StructField("open", StringType(), True),
-    StructField("high", StringType(), True),
-    StructField("low", StringType(), True),
-    StructField("close", StringType(), True),
-    StructField("volume", StringType(), True),
-    StructField("symbol", StringType(), True)
-])
+tickers_avro = """
+{
+  "type": "record", "name": "Envelope", "fields": [
+    { "name": "after", "type": ["null", { "type": "record", "name": "Value", "fields": [
+          {"name": "symbol", "type": ["null", "string"], "default": null},
+          {"name": "price_change", "type": ["null", "string"], "default": null},
+          {"name": "price_change_percent", "type": ["null", "string"], "default": null},
+          {"name": "last_price", "type": ["null", "string"], "default": null},
+          {"name": "volume", "type": ["null", "string"], "default": null}
+        ] }], "default": null }
+  ]
+}
+"""
 
-order_book_schema = StructType([
-    StructField("lastUpdateId", LongType(), True),
-    StructField("bids", StringType(), True), 
-    StructField("asks", StringType(), True),
-    StructField("symbol", StringType(), True)
-])
-
-ticker_schema = StructType([
-    StructField("symbol", StringType(), True),
-    StructField("priceChange", StringType(), True),
-    StructField("lastPrice", StringType(), True),
-    StructField("volume", StringType(), True)
-])
-
-trading_pairs_schema = StructType([
-    StructField("symbol", StringType(), True),
-    StructField("baseAsset", StringType(), True),
-    StructField("quoteAsset", StringType(), True),
-    StructField("status", StringType(), True)
-])
+pairs_avro = """
+{
+  "type": "record", "name": "Envelope", "fields": [
+    { "name": "after", "type": ["null", { "type": "record", "name": "Value", "fields": [
+          {"name": "symbol", "type": ["null", "string"], "default": null},
+          {"name": "base_asset", "type": ["null", "string"], "default": null},
+          {"name": "quote_asset", "type": ["null", "string"], "default": null},
+          {"name": "status", "type": ["null", "string"], "default": null}
+        ] }], "default": null }
+  ]
+}
+"""
