e1e37cb0fd92d35d01082643bed3beb75975f63c
cliffe <oluochcliffe13@gmail.com>
2026-06-24 23:13:48 +0300

    Fix syntax error

--- src/processing/spark_processor.py
+++ src/processing/spark_processor.py
@@ -1,27 +1,31 @@
 from src.utils.spark_utils import spark_session
 from pyspark.sql.functions import *
+from pyspark.sql.avro.functions import from_avro
 from src.utils.kafka_utils import read_kafka_topic
-import src.processing.spark_schema as schemas
+import src.processing.spark_schema 
 
 spark = spark_session()
 
 # Ingest the streams
-raw_candlesstick = read_kafka_topic("binance_server.public.candlestick_data")
+raw_candlestick = read_kafka_topic("binance_server.public.candlestick_data")
 raw_orders = read_kafka_topic("binance_server.public.order_books")
 raw_tickers = read_kafka_topic("binance_server.public.tickers")
 raw_pairs = read_kafka_topic("binance_server.public.trading_pairs")
 
+# Slice off the 5-byte Confluent Schema Registry header for all streams
+clean_avro_bytes = expr("substring(value, 6, length(value)-5)")
+
 # Enrich the cnadlestick data
 enriched_candlestick = raw_candlestick \
-    .selectExpr("CAST(value AS STRING)") \ #cast kafka bytes to string
-    .select(from_json(col("value"), schemas.wrap_debezium(schemas.candlestick_schema)).alias("data")).select("data.payload.after.*") \ #apply schema to the json data and convert to spark dataframe
+    .withColumn("avro_data", from_avro(clean_avro_bytes, candlestick_avro)) \
+    .select("avro_data.after.*") \
     .withColumn("price", col("close").cast("float")) \
     .withColumn("price_high", col("high").cast("float")) \
     .withColumn("price_low", col("low").cast("float")) \
     .withColumn("trade_volume", col("volume").cast("float")) \
-    .withColumn("timestamp", to_timestamp(col("open_time") / 1000)) \ #convert ms timestamp to seconds
-    .withWatermark("timestamp", "2 minutes") \ #2 min memory window
-    .groupBy(window(col("timestamp"), "1 minute"), col("symbol")) \ #bin event time by 60s
+    .withColumn("timestamp", to_timestamp(col("open_time") / 1000)) \
+    .withWatermark("timestamp", "2 minutes") \
+    .groupBy(window(col("timestamp"), "1 minute"), col("symbol")) \
     .agg(
         avg("price").alias("avg_minute_price"),
         sum("trade_volume").alias("total_volume_min"),
@@ -34,18 +38,18 @@ candlestick_df = enriched_candlestick \
     .withColumn("window_start", col("window.start")) \
     .drop("window")
 
-# Transform the remaining data
+# Deserialze and flatten the remaining data
 clean_orders = raw_orders \
-    .selectExpr("CAST(value AS STRING)") \
-    .select(from_json(col("value"), schemas.wrap_debezium(schemas.order_book_schema)).alias("data")).select("data.payload.after.*")
+    .withColumn("avro_data", from_avro(clean_avro_bytes, orders_avro)) \
+    .select("avro_data.after.*")
 
 clean_tickers = raw_tickers \
-    .selectExpr("CAST(value AS STRING)") \
-    .select(from_json(col("value"), schemas.wrap_debezium(schemas.ticker_schema)).alias("data")).select("data.payload.after.*")
+    .withColumn("avro_data", from_avro(clean_avro_bytes, tickers_avro)) \
+    .select("avro_data.after.*")
 
 clean_pairs = raw_pairs \
-    .selectExpr("CAST(value AS STRING)") \
-    .select(from_json(col("value"), schemas.wrap_debezium(schemas.trading_pairs_schema)).alias("data")).select("data.payload.after.*")
+    .withColumn("avro_data", from_avro(clean_avro_bytes, pairs_avro)) \
+    .select("avro_data.after.*")
 
 # Execute the streams 
 query_candles = candlestick_df.writeStream.outputMode("update") \
