5b58ce039873b40532679a368ae99f73f3e3c4f5
cliffe <oluochcliffe13@gmail.com>
2026-06-27 16:14:25 +0300

    Add batch trigger limit

--- src/utils/kafka_utils.py
+++ src/utils/kafka_utils.py
@@ -8,4 +8,5 @@ def read_kafka_topic(topic_name):
         .option("kafka.bootstrap.servers", "kafka:29092") \
         .option("subscribe", topic_name) \
         .option("startingOffsets", "earliest") \
+        .option("maxOffsetsPerTrigger", 5000) \
         .load()
