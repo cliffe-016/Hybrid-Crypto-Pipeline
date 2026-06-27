93c11217de2ce5cfb729bac2095f8f910c96fe1b
cliffe <oluochcliffe13@gmail.com>
2026-06-27 16:16:35 +0300

    Add helper function for deserialization/flattening

--- src/processing/spark_processor.py
+++ src/processing/spark_processor.py
@@ -1,24 +1,47 @@
+import requests
+import time
 from src.utils.spark_utils import spark_session
 from pyspark.sql.functions import *
 from pyspark.sql.avro.functions import from_avro
 from src.utils.kafka_utils import read_kafka_topic
-from src.processing.spark_schema import (candlestick_avro, orders_avro, tickers_avro, pairs_avro)
 
 spark = spark_session()
 
-# Ingest the streams
+def fetch_schema(topic_name, retries=20):
+    """Fetches the latest Avro schema from the Confluent Schema Registry."""
+    url = f"http://schema-registry:8081/subjects/{topic_name}-value/versions/latest"
+
+    for _try in range(retries):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            return response.json()["schema"]
+        except:
+            print(f"Schema Registry not ready. Retrying in 5 seconds... (Attempt {_try+1}/{retries})")
+            time.sleep(5)
+
+    raise Exception(f"Fatal: Could not connect to Schema Registry for {topic_name} after {retries} attempts.")
+
+
+candlestick_schema = fetch_schema("binance_server.public.candlestick_data")
+orders_schema = fetch_schema("binance_server.public.order_books")
+tickers_schema = fetch_schema("binance_server.public.tickers")
+pairs_schema = fetch_schema("binance_server.public.trading_pairs")
+
+# Ingest the kafka streams
 raw_candlestick = read_kafka_topic("binance_server.public.candlestick_data")
 raw_orders = read_kafka_topic("binance_server.public.order_books")
 raw_tickers = read_kafka_topic("binance_server.public.tickers")
 raw_pairs = read_kafka_topic("binance_server.public.trading_pairs")
 
-# Slice off the 5-byte Confluent Schema Registry header for all streams
+# Slice off the 5-byte Confluent Schema Registry header
 clean_avro_bytes = expr("substring(value, 6, length(value)-5)")
 
-# Enrich the cnadlestick data
+# Enrich and transform the data
 enriched_candlestick = raw_candlestick \
-    .withColumn("avro_data", from_avro(clean_avro_bytes, candlestick_avro)) \
+    .withColumn("avro_data", from_avro(clean_avro_bytes, candlestick_schema)) \
     .select("avro_data.after.*") \
+    .filter(col("symbol").isNotNull() & (trim(col("symbol")) != "")) \
     .withColumn("price", col("close").cast("float")) \
     .withColumn("price_high", col("high").cast("float")) \
     .withColumn("price_low", col("low").cast("float")) \
@@ -31,34 +54,57 @@ enriched_candlestick = raw_candlestick \
         sum("trade_volume").alias("total_volume_min"),
         stddev("price").alias("price_stddev"),
         (max("price_high") - min("price_low")).alias("max_spread")
-        ) 
+    ) 
 
-# Flatten the window struct so Cassandra can read it
 candlestick_df = enriched_candlestick \
     .withColumn("window_start", col("window.start")) \
     .drop("window")
 
-# Deserialze and flatten the remaining data
 clean_orders = raw_orders \
-    .withColumn("avro_data", from_avro(clean_avro_bytes, orders_avro)) \
-    .select("avro_data.after.*")
+    .withColumn("avro_data", from_avro(clean_avro_bytes, orders_schema)) \
+    .select("avro_data.after.*") \
+    .filter(col("symbol").isNotNull() & (trim(col("symbol")) != "")) \
+    .withColumnRenamed("lastUpdateId", "lastupdateid") \
+    .select(
+        col("symbol"),
+        col("lastUpdateId").alias("lastupdateid"),
+        get_json_object(translate(col("bids"), "{}", "[]"),  "$[0][1]").alias("bid_price"),
+        get_json_object(translate(col("bids"), "{}", "[]"),  "$[0][1]").alias("bid_qty"),
+        get_json_object(translate(col("asks"), "{}", "[]"),  "$[0][1]").alias("ask_price"),
+        get_json_object(translate(col("asks"), "{}", "[]"),  "$[0][1]").alias("ask_qty")
+    )
 
 clean_tickers = raw_tickers \
-    .withColumn("avro_data", from_avro(clean_avro_bytes, tickers_avro)) \
-    .select("avro_data.after.*")
+    .withColumn("avro_data", from_avro(clean_avro_bytes, tickers_schema)) \
+    .select("avro_data.after.*") \
+    .filter(col("symbol").isNotNull() & (trim(col("symbol")) != "")) \
+    .select(
+        col("symbol"),
+        col("price").alias("last_price")
+    )
 
 clean_pairs = raw_pairs \
-    .withColumn("avro_data", from_avro(clean_avro_bytes, pairs_avro)) \
-    .select("avro_data.after.*")
+    .withColumn("avro_data", from_avro(clean_avro_bytes, pairs_schema)) \
+    .select("avro_data.after.*") \
+    .filter(col("symbol").isNotNull() & (trim(col("symbol")) != "")) \
+    .select(
+        col("symbol"),
+        col("baseAsset").alias("base_asset"),
+        col("quoteAsset").alias("quote_asset"),
+        col("status")
+    )
+
 
 # Execute the streams 
-query_candles = candlestick_df.writeStream.outputMode("update") \
+query_candles = candlestick_df.writeStream.outputMode("append") \
     .format("org.apache.spark.sql.cassandra") \
     .option("keyspace", "binance") \
     .option("table", "candlestick") \
     .option("spark.cassandra.connection.host", "cassandra") \
     .option("spark.cassandra.connection.port", "9042") \
     .option("checkpointLocation", "/tmp/checkpoints/candlestick") \
+    .option("spark.cassandra.connection.timeoutMS", "60000") \
+    .trigger(processingTime="5 seconds") \
     .start()
 
 query_orders = clean_orders.writeStream.outputMode("append") \
@@ -68,7 +114,10 @@ query_orders = clean_orders.writeStream.outputMode("append") \
     .option("spark.cassandra.connection.host", "cassandra") \
     .option("spark.cassandra.connection.port", "9042") \
     .option("checkpointLocation", "/tmp/checkpoints/order_books") \
+    .option("spark.cassandra.connection.timeoutMS", "60000") \
+    .trigger(processingTime="5 seconds") \
     .start()
+
 
 query_tickers = clean_tickers.writeStream.outputMode("append") \
     .format("org.apache.spark.sql.cassandra") \
@@ -77,6 +126,8 @@ query_tickers = clean_tickers.writeStream.outputMode("append") \
     .option("spark.cassandra.connection.host", "cassandra") \
     .option("spark.cassandra.connection.port", "9042") \
     .option("checkpointLocation", "/tmp/checkpoints/tickers") \
+    .option("spark.cassandra.connection.timeoutMS", "60000") \
+    .trigger(processingTime="5 seconds") \
     .start()
 
 query_pairs = clean_pairs.writeStream.outputMode("append") \
@@ -86,6 +137,8 @@ query_pairs = clean_pairs.writeStream.outputMode("append") \
     .option("spark.cassandra.connection.host", "cassandra") \
     .option("spark.cassandra.connection.port", "9042") \
     .option("checkpointLocation", "/tmp/checkpoints/trading_pairs") \
+    .option("spark.cassandra.connection.timeoutMS", "60000") \
+    .trigger(processingTime="5 seconds") \
     .start()
 
 spark.streams.awaitAnyTermination()
